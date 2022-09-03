6fbe07b86cf22c80927f428910f9df5d8f8987cc
Serhii <ewentualnie.sz@gmail.com>
2022-09-03 19:29:44 +0300

    add MyStack

--- src/main/java/MyStack.java
+++ src/main/java/MyStack.java
@@ -1,2 +1,33 @@
-public class MyStack {
+public class MyStack<T> {
+    private final MyArrayList<T> array;
+
+    public MyStack() {
+        array = new MyArrayList<>();
+    }
+
+    public void push(T object) {
+        array.add(object);
+    }
+
+    public void remove(int index) {
+        array.remove(index);
+    }
+
+    public void clear() {
+        array.clear();
+    }
+
+    public int size() {
+        return array.size();
+    }
+
+    public T peek() {
+        return array.get(array.size() - 1);
+    }
+
+    public T poll() {
+        T object = array.get(array.size() - 1);
+        array.remove(array.size() - 1);
+        return object;
+    }
 }
